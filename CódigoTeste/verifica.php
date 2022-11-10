2b95541fe9013462e4ada076f79f7df3aaa39bb4
Gustavo Henrique Tavares de Souza <gutvo9@hotmail.com>
2022-11-10 19:06:05 -0300

    Verificar

---
+++ CódigoTeste/verifica.php
@@ -0,0 +1,12 @@
+<?php
+// Inicia sessões
+session_start();
+
+// Verifica se existe os dados da sessão de login
+if(!isset($_SESSION["ID"]) || !isset($_SESSION["nomeusuario"]))
+{
+// Usuário não logado! Redireciona para a página de login
+header("Location: login.php");
+exit;
+}
+?>
