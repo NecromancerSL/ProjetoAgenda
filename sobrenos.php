efefa5c76dc6003f22237bed00fb0dc4bad28d37
yuri <yurifurgeri@gmail.com>
2022-11-22 20:17:43 -0300

    add sobre nos

--- sobrenos.php
+++ sobrenos.php
@@ -1,53 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <?php include "verificar.php";?>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <script src='js/fullcalendar/main.js'></script>
-    <script src='js/fullcalendar/locales/pt-br.js'></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src='js/fullcalendar/main.js'></script>
+        <script src='js/fullcalendar/locales/pt-br.js'></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.1/font/bootstrap-icons.css">
-    <script src="https://kit.fontawesome.com/8b69b9518f.js" crossorigin="anonymous"></script>
-    <link href='css/fullcalendar/main.css' rel='stylesheet' />
-    <link rel="stylesheet" href="css/estilo.css">
-    <title>Home</title>
-    <script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.1/font/bootstrap-icons.css">
+        <script src="https://kit.fontawesome.com/8b69b9518f.js" crossorigin="anonymous"></script>
+        <link href='css/fullcalendar/main.css' rel='stylesheet' />
+        <link rel="stylesheet" href="css/estilo.css">
+        <title>Tutorial</title>
+    </head>
 
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            headerToolbar: {
-                right: 'prev,next today',
-                left: 'title'
-            },
-            themeSystem: 'bootstrap5',
-            locale: 'pt-br',
-            editable: true,
-            dayMaxEvents: true,
-            events: 'calendario.php',
-            eventClick: function(info) {
-            var eventObj = info.event;
-                window.location.href = "agendamento_formulario_editar.php?id="+eventObj.id;
-            }
-        });
-
-        calendar.render();
-
-
-    });
-    </script>
-</head>
-
-<body>
+    <body>
     <a name="topo">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="perfil.php">Olá,
-                    <?php echo strtok($_SESSION["nomeusuario"], " ");?></a>
+                <a class="navbar-brand" href="perfil.php"><?php echo strtok($_SESSION["nomeusuario"], " ");?></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -71,18 +44,29 @@
                 </div>
             </div>
         </nav>
-        <div class="container p-4">
-            <h1 class="titulo" id="texto">Calendário</h1>
-            <div id='calendar'>
-
-            </div>
-            <br>
-            <div class="row  justify-content-center align-items-center">
-                <a href="agendamentos.php"><button type="submit" class="btn btn-dark botao">Agendamentos</button>
-                </a>
-            </div>
+        
+        <div class="text-center">
+        <h1>GRUPO 2</h1>
         </div>
 
+        <div class="d-flex flex-column align-items-center">
+        <img width="800px"height="600px" src="imagens/imagemgrupo2.png">
+        </div>
+
+        <div>
+        <h2>Sobre Nós :</h2>
+        <h3>O grupo 2 foi criado para a matéria de Desenvolvimento de Software II na Faculdade Eduvale Avaré, sendo constituído por Gustavo Nunes, Gustavo Tavares e Yuri Furgeri, o grupo 2 tem como objetivo principal a entrega de um serviço de qualidade e de fácil acesso, sendo de suma importancia a comunicação entre os integrantes o grupo 2 se destaca pela união e excelência na execução do projeto.</h3>    
+        </div>
+        <div>
+        <h2>Agenda Feliz :</h2>
+        <h3>A agenda feliz é uma Web Agenda para uso geral, sendo adequada para todos os públicos e de fácil uso a Agenda Feliz se destaca pelo seu design minimalista e direto ao ponto, para facilitar a acessibilidade e entregar maior desempenho. As tarefas são vistas por ordem de importância e urgencia trazendo uma experiência visual de fácil entendimento. A Agenda Feliz não só proporciona agendamentos e horários, mas uma proposta de organização e produtividade para o dia a dia de todo usuário.</h3>    
+        </div>
+        <div>
+        <h2>Sobre o Projeto:</h2>
+        <h3>O projeto passa por todas as etapas do desenvolvimento de um software, desde o planejamento e documentação até a programação, testes e entrega. Com bastante foco no trabalho em equipe e nos procedimentos internos, a matéria visa demonstrar uma visão mais próxima da realidade e do dia a dia de um desenvolvedor e as etapas necessárias para a implementação de um projeto do zero.</h3>    
+        </div>
+        
+            
         <footer class="bg-dark text-center text-white">
             <!-- Grid container -->
             <div class="container p-4">
@@ -106,7 +90,7 @@
                         Site criado como projeto de faculdade
                     </p>
                 </section>
-                <section class="">
+                <section>
                     <div class="row">
                         <div class="mb-4">
 
@@ -123,12 +107,8 @@
                 </section>
             </div>
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                © 2022 Copyright: <a class="text-white" href="sobrenos.php">Grupo 2</a>
+                © 2022 Copyright: <a class="text-white" href="#">Grupo 2</a>
             </div>
         </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
-        </script>
-</body>
-
+    </body>
 </html>
